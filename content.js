966ef622d71d3c2a7c46bf38a274c704c2d45e7b
g.ferrer <gustavo.ferrer@totvs.com.br>
2022-04-07 13:13:46 -0300

    better performance

--- content.js
+++ content.js
@@ -1,21 +1,19 @@
 const puppeteer = require("puppeteer");
 
 const OPTION_NUMBER = 4;
-const DELAY_IN_MILISECONDS = 500;
+const FORM_LINK =
+  "https://docs.google.com/forms/d/e/1FAIpQLSdZDiHCztvOE4SOICzZBPAMWfdYnbMm5WVPiHedSjB-6y0Klg/viewform";
 
 class Content {
   async start() {
     const browser = await puppeteer.launch({ headless: true });
 
     const page = await browser.newPage();
-    await page.goto(
-      "https://docs.google.com/forms/d/e/1FAIpQLSdZDiHCztvOE4SOICzZBPAMWfdYnbMm5WVPiHedSjB-6y0Klg/viewform",
-      {
-        waitUntil: "networkidle0",
-      }
-    );
+    await page.goto(FORM_LINK, {
+      waitUntil: "networkidle0",
+    });
 
-   var contador = 0;
+    var contador = 0;
 
     while (1 === 1) {
       try {
@@ -23,30 +21,26 @@ class Content {
           (await page.waitForSelector("span.aDTYNe")) &&
           (await page.waitForSelector("span.NPEfkd"))
         ) {
-          await page.waitForTimeout(DELAY_IN_MILISECONDS);
-
           const options = await page.$$("span.aDTYNe");
           options[OPTION_NUMBER - 1].click();
-
-          await page.waitForTimeout(DELAY_IN_MILISECONDS);
 
           const button = await page.$$("span.NPEfkd");
           button[0].click();
         }
 
-        await page.waitForTimeout(DELAY_IN_MILISECONDS + 200);
-
-        await page.goto(
-          "https://docs.google.com/forms/d/e/1FAIpQLSdZDiHCztvOE4SOICzZBPAMWfdYnbMm5WVPiHedSjB-6y0Klg/viewform",
-          {
+        if (await page.waitForSelector("div.pdLVYe")) {
+          await page.goto(FORM_LINK, {
             waitUntil: "networkidle0",
-          }
-        );
-
-	contador++;
-        console.log("VOTAÇÃO CONCLUÍDA::", contador);
+          });
+        }
+        contador++;
+        console.log("VOTO COMPUTADO::", contador);
       } catch (err) {
-        console.log("erro");
+        console.log("Erro - Redirecionando você para a página inicial");
+
+        await page.goto(FORM_LINK, {
+          waitUntil: "networkidle0",
+        });
       }
     }
   }
