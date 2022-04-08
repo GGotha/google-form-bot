cf1c3ae60c95c2972adb073fc16322e0af65bdbd
g.ferrer <gustavo.ferrer@totvs.com.br>
2022-04-08 20:15:59 -0300

    set count only when request is sent

--- web/src/App.js
+++ web/src/App.js
@@ -7,11 +7,12 @@ function App() {
   const [currentInterval, setCurrentInterval] = useState(3000);
 
   const vote = useCallback(async () => {
-    setCount((count) => count + 1);
     try {
       await axios.post(
         "https://docs.google.com/forms/d/e/1FAIpQLSdZDiHCztvOE4SOICzZBPAMWfdYnbMm5WVPiHedSjB-6y0Klg/formResponse?entry.1135870437=Op%C3%A7%C3%A3o%204%20%E2%80%93%20Samsung%20versus%20Apple&draftResponse=%5B1,null,%5B%5Bnull,null,%5B%22Op%C3%A7%C3%A3o%204%20%E2%80%93%20Samsung%20versus%20Apple%22%5D,false,null,null,1135870437%5D%5D,null,%5B%22-9015358315957581321%22%5D%5D"
       );
+
+      setCount((count) => count + 1);
     } catch (err) {
       console.log("erro ao computar voto");
     }
